202cd3c74ca23aab924022f7c5599678ab01ae9e
youngrrrr <young-rae_kim@brown.edu>
2016-11-25 22:47:49 -0500

    Provide notice when sending pain has begun

--- zwrite_infinite.sh
+++ zwrite_infinite.sh
@@ -3,6 +3,7 @@ zero=0
 while [ 1 = 1 ]; do
 	isLoggedIn="$(rwho > zwrite_infinite.txt | grep $1 zwrite_infinite.txt > zwrite_infinite2.txt | cat zwrite_infinite2.txt | wc -l)"
 	if [ "$isLoggedIn" -gt "$zero" ]; then
+		echo "sending the pain to $1"
 		while [ 1 = 1 ]; do
 			zwrite -q -n $1 <<< $2 \n .
 			sleep $3
